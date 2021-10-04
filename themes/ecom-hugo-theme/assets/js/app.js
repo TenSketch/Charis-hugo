efb474fb26972ffc86e8333ce15685a13cc586bf
TenSketch <bala@tensketch.com>
2021-10-04 12:33:37 +0530

    thumbnail slider done static

--- themes/ecom-hugo-theme/assets/js/app.js
+++ themes/ecom-hugo-theme/assets/js/app.js
@@ -48,3 +48,36 @@ var swiper = new Swiper('.swiper-container', {
 
 // Initialize Animate on scroll
 AOS.init();
+
+
+// product images thumbnail slider 
+let thumbnails = document.getElementsByClassName('thumbnail')
+
+let activeImages = document.getElementsByClassName('active')
+
+for (var i = 0; i < thumbnails.length; i++) {
+
+  thumbnails[i].addEventListener('mouseover', function () {
+    console.log(activeImages)
+
+    if (activeImages.length > 0) {
+      activeImages[0].classList.remove('active')
+    }
+
+
+    this.classList.add('active')
+    document.getElementById('featured').src = this.src
+  })
+}
+
+
+let buttonRight = document.getElementById('slideRight');
+let buttonLeft = document.getElementById('slideLeft');
+
+buttonLeft.addEventListener('click', function () {
+  document.getElementById('prod_slider').scrollLeft -= 180
+})
+
+buttonRight.addEventListener('click', function () {
+  document.getElementById('prod_slider').scrollLeft += 180
+})
